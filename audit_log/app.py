e8a00f646e68cf2bb1a7f8015f9dde63dad0e372
tan nguyen <tannguyen901.tannguyen901@gmail.com>
2021-11-22 21:45:28 -0800

    lab10 stuff--beginning

--- audit_log/app.py
+++ audit_log/app.py
@@ -11,6 +11,14 @@ from pykafka.common import OffsetType
 from threading import Thread
 from flask_cors import CORS, cross_origin
 
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+    print("In Test Environment")
+    app_conf_file = "/config/app_conf.yml"
+    log_conf_file = "/config/log_conf.yml"
+else:
+    print("In Dev Environment")
+    app_conf_file = "app_conf.yml"
+    log_conf_file = "log_conf.yml"
 
 with open ('app_conf.yml', 'r') as f:
     app_config= yaml.safe_load(f.read())
@@ -20,6 +28,8 @@ with open('log_conf.yml','r') as f:
     logging.config.dictConfig(log_config)
 
 logger = logging.getLogger('basicLogger')
+logger.info("App Conf File: %s" % app_conf_file)
+logger.info("Log Conf File: %s" % log_conf_file)
 
 def get_students(index):
     """ Get student Reading in History """
