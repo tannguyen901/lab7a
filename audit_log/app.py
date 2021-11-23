c8dc415e5cea17e00a7fdc7d7f5fd9f82b3c2cf2
tan nguyen <tannguyen901.tannguyen901@gmail.com>
2021-11-23 09:05:23 -0800

    import os

--- audit_log/app.py
+++ audit_log/app.py
@@ -10,6 +10,7 @@ from pykafka import KafkaClient
 from pykafka.common import OffsetType
 from threading import Thread
 from flask_cors import CORS, cross_origin
+import os
 
 if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
     print("In Test Environment")
